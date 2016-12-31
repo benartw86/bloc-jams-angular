04dbff7259dbd9dc87164d764e5a99519e42f17c
Ben White <masinfa@gmail.com>
2016-12-30 19:09:22 -0500

    Mods to hrefs

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,31 @@
-angular.module('blocJams', ['ui.router']);  //the second argument that is an array is called the dependency injection, for listing external modules
+(function) {
+    function config($stateProvider, $locationProvider) {
+        $locationProvider
+            .html5Mode({
+                enabled: true,
+                requireBase: false
+            });
+        
+        $stateProvider                  //becuase $stateProvider.state() returns $stateProvider, we are able to call state() again without having to reference the $stateProvider, called method chaining
+            .state('landing', {
+                url: '/',
+                templateUrl: '/templates/landing.html'
+            })
+            .state('album', {
+                url: '/album',
+                templateUrl: '/templates/album.html'
+            })
+            .state('collection', {
+                url:'/collection',
+                templateUrl: '/templates/collection.html'
+            });
+    }
+    
+    angular
+        .module('blocJams', ['ui.router'])
+        .config(config);    
+})();
+
+
+
+//the second argument that is an array is called the dependency injection, for listing external modules
