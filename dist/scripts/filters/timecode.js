388d1b9ff38576655e7fff59b448649fcba5facb
Ben White <masinfa@gmail.com>
2017-01-18 19:45:04 -0500

    Checkpoint 11, Filters, Done, buzz.toTimer implemented in filter

---
+++ dist/scripts/filters/timecode.js
@@ -0,0 +1,14 @@
+(function() {
+    
+    function timecode() {
+        return function(seconds) {
+            var timer = buzz.toTimer(seconds)
+            return timer;
+        };
+
+    }
+ 
+    angular
+      .module('blocJams')
+      .filter('timecode', timecode);
+})();
